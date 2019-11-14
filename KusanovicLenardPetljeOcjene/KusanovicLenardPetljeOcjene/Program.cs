e5b2bd85b7cc62afa90931a6e221c4c4ec1bb1f1
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2019-11-14 11:30:34 +0100

    Add files via upload

---
+++ KusanovicLenardPetljeOcjene/KusanovicLenardPetljeOcjene/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KusanovicLenardPetljeOcjene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ocjena;
+            float prosjek;
+            float brojOcjena = 0;
+            int zbrojOcjena = 0;
+
+            do
+            {
+
+
+
+
+                Console.WriteLine("Upisite ocjenu: ");
+                ocjena = Convert.ToInt16(Console.ReadLine());
+               
+                
+
+                 if (ocjena == 1)
+                {
+                    prosjek = 1;
+                    break;
+                }
+
+
+                else if (ocjena > 5 || ocjena < 0)
+                {
+                    Console.WriteLine("Pogresan unos unesite ponovo ");
+                    
+                }
+                else if (ocjena !=0){
+                    brojOcjena++;
+                    zbrojOcjena +=ocjena;
+
+
+                    
+
+                }
+                prosjek = zbrojOcjena / brojOcjena;
+            }
+            while (ocjena != 0);
+
+            Console.WriteLine("Prosjek ocjena je: "+ prosjek);
+            Console.ReadKey();
+
+        }
+    }
+}
